1471157ae5b6e4f0e2d9cfa30c2e808da4e7d010
Lukas Ruebbelke <simpul@gmail.com>
2017-10-04 06:28:27 -0700

    update: adding in primeng

---
+++ src/app/app-primeng.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {
+  ButtonModule,
+  CheckboxModule,
+  InputTextModule
+} from 'primeng/primeng';
+
+@NgModule({
+  imports: [
+    ButtonModule,
+    CheckboxModule,
+    InputTextModule
+  ],
+  exports: [
+
+  ]
+})
+export class AppPrimengModule { }
