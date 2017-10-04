a8893d34cd620afd9e0631443319f8571cc6b9b2
Lukas Ruebbelke <simpul@gmail.com>
2017-10-03 22:52:08 -0700

    update: adding in local instance

--- src/app/shared/apollo.client.ts
+++ src/app/shared/apollo.client.ts
@@ -1,6 +1,10 @@
 import { ApolloClient, createNetworkInterface } from 'apollo-client';
 
+// Graph.cool instance
 const networkInterface = createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj864jf2302n30112ip74zkoy' });
+
+// Local Instance
+// const networkInterface = createNetworkInterface({ uri: 'http://localhost:8080/graphql' });
 
 const client = new ApolloClient({ networkInterface });
 
