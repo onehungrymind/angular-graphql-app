5f8dc804d01fbd5c25430c16b46878d1c7431a36
Lukas Ruebbelke <simpul@gmail.com>
2017-10-06 11:26:03 -0700

    update: adding subscriptions BROKEN

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CoursesService } from '../shared/courses.service';
 import { StudentsService } from '../shared/students.service';
 import { Course } from '../shared/course.model';
 import { Student } from '../shared/student.model';
+import { Subscription } from 'rxjs/Subscription';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +13,15 @@ import { Student } from '../shared/student.model';
   styleUrls: ['./home.component.css'],
 
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   courses: Course[];
   students: Student[];
+  courses$: Subscription;
 
-  constructor(private coursesService: CoursesService, private studentsService: StudentsService) {
+  constructor(
+    private apollo: Apollo,
+    private coursesService: CoursesService,
+    private studentsService: StudentsService) {
   }
 
   ngOnInit() {
@@ -22,13 +29,41 @@ export class HomeComponent implements OnInit {
     this.getStudents();
   }
 
+  initSubscription(subscription) {
+    this.apollo.subscribe({
+      query: gql`
+      subscription {
+        Course(filter: {mutation_in: [CREATED, UPDATED, DELETED]}) {
+          node {
+            id
+            name
+            description
+          }
+        }
+      }`,
+      variables: {}
+    })
+    .subscribe({
+      next (data) {
+        subscription.refetch();
+      }
+    })
+    ;
+  }
+
   getCourses() {
-    this.coursesService.all()
+    this.courses$ = this.coursesService.all()
       .subscribe(courses => this.courses = courses);
+
+    this.initSubscription(this.courses$);
   }
 
   getStudents() {
     this.studentsService.all()
       .subscribe(students => this.students = students);
+  }
+
+  ngOnDestroy() {
+    this.courses$.unsubscribe();
   }
 }
--- src/app/shared/apollo.client.ts
+++ src/app/shared/apollo.client.ts
@@ -1,12 +1,28 @@
 import { ApolloClient, createNetworkInterface } from 'apollo-client';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { addGraphQLSubscriptions } from 'add-graphql-subscriptions';
 
-// Graph.cool instance
-const networkInterface = createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj864jf2302n30112ip74zkoy' });
+const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'wss://subscriptions.us-west-2.graph.cool/v1/cj864jf2302n30112ip74zkoy';
+const GRAPHQL_ENDPOINT = 'https://api.graph.cool/simple/v1/cj864jf2302n30112ip74zkoy';
+const EXPRESS_ENDPOINT = 'http://localhost:8080/graphql';
 
-// Local Instance
-// const networkInterface = createNetworkInterface({ uri: 'http://localhost:8080/graphql' });
+const wsClient = new SubscriptionClient(GRAPHQL_SUBSCRIPTION_ENDPOINT, {
+  reconnect: true
+});
 
-const client = new ApolloClient({ networkInterface });
+// Graph.cool Instance
+const networkInterface = createNetworkInterface({ uri: GRAPHQL_ENDPOINT });
+
+// Extend the network interface with the WebSocket
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+);
+
+// Finally, create your ApolloClient instance with the modified network interface
+const client = new ApolloClient({
+  networkInterface: networkInterfaceWithSubscriptions
+});
 
 export function provideClient(): ApolloClient {
   return client;
