bef916d94c669ec3a27450826e98be0dd55277b5
Ryan Chenkie <ryanchenkie@gmail.com>
2017-10-09 21:14:08 -0500

    rename type Course to CourseType

--- src/app/shared/students.service.ts
+++ src/app/shared/students.service.ts
@@ -10,7 +10,7 @@ import 'rxjs/add/operator/switchMap';
 
 const fragments = {
   courses: gql`
-    fragment courseFields on Course {
+    fragment courseFields on CourseType {
       id
       name
       description
@@ -35,42 +35,17 @@ const AllStudentsQuery = gql`
 `;
 
 const CreateStudentMutation = gql`
-  mutation($firstName: String!, $lastName: String!, $active: Boolean!, $coursesIds: [ID!]!) {
-    createStudent (
+  mutation(
+    $firstName: String!
+    $lastName: String!
+    $active: Boolean!
+    $coursesIds: [ID!]!
+  ) {
+    createStudent(
       firstName: $firstName
       lastName: $lastName
       active: $active
       coursesIds: $coursesIds
-  ) {
-      id
-      firstName
-      lastName
-      active
-    }
-  }
-`;
-
-const UpdateStudentMutation = gql`
-  mutation($id:ID!, $firstName: String!, $lastName: String!, $active: Boolean!, $coursesIds: [ID!]!) {
-    updateStudent (
-      id: $id
-      firstName: $firstName
-      lastName: $lastName
-      active: $active
-      coursesIds: $coursesIds
-  ) {
-      id
-      firstName
-      lastName
-      active
-    }
-  }
-`;
-
-const DeleteStudentMutation = gql`
-  mutation($id:ID!) {
-    deleteStudent (
-      id: $id
     ) {
       id
       firstName
@@ -80,86 +55,131 @@ const DeleteStudentMutation = gql`
   }
 `;
 
+const UpdateStudentMutation = gql`
+  mutation(
+    $id: ID!
+    $firstName: String!
+    $lastName: String!
+    $active: Boolean!
+    $coursesIds: [ID!]!
+  ) {
+    updateStudent(
+      id: $id
+      firstName: $firstName
+      lastName: $lastName
+      active: $active
+      coursesIds: $coursesIds
+    ) {
+      id
+      firstName
+      lastName
+      active
+    }
+  }
+`;
+
+const DeleteStudentMutation = gql`
+  mutation($id: ID!) {
+    deleteStudent(id: $id) {
+      id
+      firstName
+      lastName
+      active
+    }
+  }
+`;
+
 interface QueryResponse {
-  allStudents
+  allStudents;
 }
 
 @Injectable()
 export class StudentsService {
-  constructor(private apollo: Apollo, private coursesService: CoursesService) {
-  }
+  constructor(private apollo: Apollo, private coursesService: CoursesService) {}
 
   all() {
-    return this.apollo.watchQuery<QueryResponse>({
+    return this.apollo
+      .watchQuery<QueryResponse>({
         query: AllStudentsQuery
       })
-      .map(({data}) => data.allStudents);
+      .map(({ data }) => data.allStudents);
   }
 
   full() {
-    return this.all()
-      .switchMap(students => this.coursesService.all()
-        .map(courses => students.map(student => this.transformStudent(student, courses)))
-      );
+    return this.all().switchMap(students =>
+      this.coursesService
+        .all()
+        .map(courses =>
+          students.map(student => this.transformStudent(student, courses))
+        )
+    );
   }
 
   create(student: Student) {
     return this.apollo.mutate({
-        mutation: CreateStudentMutation,
-        variables: {
-          firstName: student.firstName,
-          lastName: student.lastName,
-          active: student.active,
-          coursesIds: this.parseCourseIds(student.courses)
-        },
-        refetchQueries: [{
+      mutation: CreateStudentMutation,
+      variables: {
+        firstName: student.firstName,
+        lastName: student.lastName,
+        active: student.active,
+        coursesIds: this.parseCourseIds(student.courses)
+      },
+      refetchQueries: [
+        {
           query: AllStudentsQuery
-        }]
-      });
+        }
+      ]
+    });
   }
 
   update(student: Student) {
     return this.apollo.mutate({
-        mutation: UpdateStudentMutation,
-        variables: {
-          id: student.id,
-          firstName: student.firstName,
-          lastName: student.lastName,
-          active: student.active,
-          coursesIds: this.parseCourseIds(student.courses)
-        },
-        refetchQueries: [{
+      mutation: UpdateStudentMutation,
+      variables: {
+        id: student.id,
+        firstName: student.firstName,
+        lastName: student.lastName,
+        active: student.active,
+        coursesIds: this.parseCourseIds(student.courses)
+      },
+      refetchQueries: [
+        {
           query: AllStudentsQuery
-        }]
-      });
+        }
+      ]
+    });
   }
 
   delete(student: Student) {
     return this.apollo.mutate({
-        mutation: DeleteStudentMutation,
-        variables: {
-          id: student.id
-        },
-        refetchQueries: [{
+      mutation: DeleteStudentMutation,
+      variables: {
+        id: student.id
+      },
+      refetchQueries: [
+        {
           query: AllStudentsQuery
-        }]
-      });
+        }
+      ]
+    });
   }
 
   private parseCourseIds(courses: Course[]) {
-    return courses
-      .filter(course => course.enrolled)
-      .map(course => course.id);
+    return courses.filter(course => course.enrolled).map(course => course.id);
   }
 
   private transformStudent(student: Student, allCourses: Course[]) {
-    return Object.assign({}, student, { courses: this.buildCourses(student.courses, allCourses)});
+    return Object.assign({}, student, {
+      courses: this.buildCourses(student.courses, allCourses)
+    });
   }
 
   private buildCourses(enrolledCourses: Course[], allCourses: Course[]) {
     return allCourses.map(course => {
-      return Object.assign({}, course, {enrolled: this.isEnrolled(course, enrolledCourses)})
-    })
+      return Object.assign({}, course, {
+        enrolled: this.isEnrolled(course, enrolledCourses)
+      });
+    });
   }
 
   private isEnrolled(course: Course, enrolledCourses: Course[]) {
